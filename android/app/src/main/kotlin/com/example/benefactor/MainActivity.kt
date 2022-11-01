bf5e8198a8670dac4652d3f05d36e5bd10f785dd
fathim09 <fthimahmoud4@gmail.com>
2022-11-01 14:48:49 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/benefactor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.benefactor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splach/splachcreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Splachcreen extends StatefulWidget {
+  const Splachcreen({super.key});
+
+  @override
+  State<Splachcreen> createState() => _SplachcreenState();
+}
+
+class _SplachcreenState extends State<Splachcreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
