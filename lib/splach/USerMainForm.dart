1d114ffa26f26fa267279c48f0869e169ac6291c
fathim09 <fthimahmoud4@gmail.com>
2022-11-01 18:18:40 +0200

    Final Level one

--- lib/splach/USerMainForm.dart
+++ lib/splach/USerMainForm.dart
@@ -10,9 +10,12 @@ class MainForm extends StatefulWidget {
 
 // ignore: prefer_const_constructors
 int index = 0;
+final Pages = [
+//  محدش يقربلها بعد اذنكم عشان هكمل هنا
+];
 
 class _MainFormState extends State<MainForm> {
-  int index = 0;
+  int index = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +37,22 @@ class _MainFormState extends State<MainForm> {
           IconButton(
               icon: const Icon(Icons.exit_to_app),
               padding: const EdgeInsets.only(right: 20),
-              onPressed: () {}),
+              onPressed: () {
+// زر لتسجيل الخروج
+                showBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      // ignore: sized_box_for_whitespace
+                      return Container(
+                          height: 400,
+                          child: Center(
+                              // child: ElevatedButton(
+                              //   child: Text('خروج '),
+
+                              // ),
+                              ));
+                    });
+              }),
         ],
         flexibleSpace: Container(
             // ignore: prefer_const_constructors
@@ -51,29 +69,51 @@ class _MainFormState extends State<MainForm> {
             ]))),
       ),
 
-////  ده ال Nagivation Bar  الزراير اللي تحت 
+////  ده ال Nagivation Bar  الزراير اللي تحت
 
       // ignore: prefer_const_literals_to_create_immutables
       bottomNavigationBar: NavigationBar(
           selectedIndex: index,
           onDestinationSelected: (index) => setState(() => this.index = index),
           // ignore: prefer_const_literals_to_create_immutables
-          height: 60,
+          height: 50,
+          backgroundColor: Colors.cyan,
           // ignore: prefer_const_literals_to_create_immutables, duplicate_ignore
           destinations: [
             // ignore: prefer_const_constructors
-            NavigationDestination(
-                icon: const Icon(Icons.home_filled),
-                selectedIcon: const Icon(Icons.home_outlined),
-                label: " الصفحة الرئيسية"),
+
             // ignore: prefer_const_constructors
             NavigationDestination(
-                icon: const Icon(Icons.category),
-                selectedIcon: const Icon(Icons.category_outlined),
+                icon: const Icon(
+                  Icons.category,
+                  color: Colors.white,
+                ),
+                selectedIcon: const Icon(
+                  Icons.category_outlined,
+                  color: Colors.white,
+                ),
                 label: "  الأقسام "),
+
             NavigationDestination(
-                icon: const Icon(Icons.person),
-                selectedIcon: const Icon(Icons.person_outlined),
+                icon: const Icon(
+                  Icons.home_filled,
+                  color: Colors.white,
+                ),
+                selectedIcon: const Icon(
+                  Icons.home_outlined,
+                  color: Colors.white,
+                ),
+                label: " الصفحة الرئيسية"),
+
+            NavigationDestination(
+                icon: const Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                selectedIcon: const Icon(
+                  Icons.person_outlined,
+                  color: Colors.white,
+                ),
                 label: "  الملف الشخصي  "),
           ]),
 
@@ -88,7 +128,7 @@ class _MainFormState extends State<MainForm> {
                 child: DrawerHeader(
                   padding: EdgeInsets.only(top: 50, right: 15),
                   child: Text(
-                    '   فاعل خير    ',
+                    '        الخدمات   \n (  اسم المؤسسه ) ',
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 35,
@@ -96,34 +136,17 @@ class _MainFormState extends State<MainForm> {
                   ),
                 ),
               ),
-              Container(color: Colors.purple),
 
               ///////////////// الحالات "
               ///////////////////////////////////
               ListTile(
                 title: const Text(
-                  " الحالات ",
+                  "   الحالات ",
                   style: TextStyle(color: Colors.white),
                 ),
                 // ignore: prefer_const_constructors
                 leading: Icon(
-                  Icons.person_add_alt_sharp,
-                  color: Colors.black45,
-                ),
-                //,  عند الضغط هيعمل كذه
-                onTap: () {},
-              ),
-
-              ///////////////// الخدمات اليومية"
-              ///////////////////////////////////
-              ListTile(
-                title: const Text(
-                  " الخدمات اليومية",
-                  style: TextStyle(color: Colors.white),
-                ),
-                // ignore: prefer_const_constructors
-                leading: Icon(
-                  Icons.person_add_alt_sharp,
+                  Icons.personal_video,
                   color: Colors.black45,
                 ),
                 //,  عند الضغط هيعمل كذه
@@ -134,12 +157,42 @@ class _MainFormState extends State<MainForm> {
               ///////////////////////////////////
               ListTile(
                 title: const Text(
-                  "التبرعات ",
+                  "  التبرعات ",
                   style: TextStyle(color: Colors.white),
                 ),
                 // ignore: prefer_const_constructors
                 leading: Icon(
-                  Icons.person_add_alt_sharp,
+                  Icons.volunteer_activism,
+                  color: Colors.black45,
+                ),
+                //,  عند الضغط هيعمل كذه
+                onTap: () {},
+              ),
+              ///////////////// الزيارات "
+              ///////////////////////////////////
+              ListTile(
+                title: const Text(
+                  "  الزيارات ",
+                  style: TextStyle(color: Colors.white),
+                ),
+                // ignore: prefer_const_constructors
+                leading: Icon(
+                  Icons.person_pin_circle_rounded,
+                  color: Colors.black45,
+                ),
+                //,  عند الضغط هيعمل كذه
+                onTap: () {},
+              ),
+              ///////////////// الخدمات اليومية"
+              ///////////////////////////////////
+              ListTile(
+                title: const Text(
+                  " الخدمات اليومية",
+                  style: TextStyle(color: Colors.white),
+                ),
+                // ignore: prefer_const_constructors
+                leading: Icon(
+                  Icons.design_services,
                   color: Colors.black45,
                 ),
                 //,  عند الضغط هيعمل كذه
@@ -149,11 +202,22 @@ class _MainFormState extends State<MainForm> {
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.amber,
+        label: Text(
+          'إضافة حاله ',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        icon: Icon(Icons.person_add_alt),
+        onPressed:
+            () {}, //  عند الضغط ع الزر هينقلني لصفحة جديده عشان اسجل الحاله
+        hoverColor: Colors.grey[200],
+        extendedTextStyle: TextStyle(color: Colors.black),
+      ),
+
+///// المعلومات الاساسية في الصفحة
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          // ignore: prefer_const_constructors
-        ],
+        children: [],
       ),
     );
   }
